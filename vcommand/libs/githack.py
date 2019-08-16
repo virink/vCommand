d760ad34a8b36147b7c67a6977d857875d768863
Virink <virink@outlook.com>
2019-08-16 12:26:07 +0800

    Fix: plugin githack:_fix_missing

--- vcommand/libs/githack.py
+++ vcommand/libs/githack.py
@@ -8,9 +8,7 @@ import os
 import sys
 import re
 import time
-import threading
 import requests
-import asyncio
 
 
 try:
@@ -49,43 +47,30 @@ BASE_FILES = [
     '.git/COMMIT_EDITMSG'
 ]
 
-q = asyncio.Queue(maxsize=MAX_QUEUE_NUM)
-
-
-async def _async_download_file(q):
-    while not q.empty():
-        f = await q.get()
-        _download_file(f['u'], f['p'])
-        await asyncio.sleep(0.05)
-
 
 def _get_sha1(content):
     result = re.findall(r"([a-fA-F0-9]{40})", content)
     return result
 
 
-def _fix_missing(baseurl, temppath):
+def _fix_missing(baseurl, temppath, origin_missing=[]):
+    # TODO: Optimization
     global q
-    os.system("cd ./%s && git fsck > ../cache.dat 2>&1" % temppath)
+    os.system("cd ./%s && git fsck --full > ../cache.dat 2>&1" % temppath)
     missing = []
-    if os.path.getsize("./cache.dat") < 5:
-        cprint(CRED, "[-] Missing Objects Not Found")
-        return False
+    # if os.path.getsize("./cache.dat") < 5:
+    #     cprint(CRED, "[-] Missing Objects Not Found")
+    #     return False
     with open("./cache.dat", "r") as f:
         missing += _get_sha1(f.read())
-    length = len(missing)
     os.unlink("./cache.dat")
     for i in missing:
         path = "./%s/.git/objects/%s/%s" % (temppath, i[0:2], i[2:])
         url = "%sobjects/%s/%s" % (baseurl, i[0:2], i[2:])
-        q.put_nowait({"u": url, "p": path})
-    if length > 1:
+        _download_file(url, path)
+    if len(missing) > 1:
         return _fix_missing(baseurl, temppath)
     else:
-        loop = asyncio.get_event_loop()
-        tasks = [_async_download_file(q) for i in range(NUMBER_OF_THREAD)]
-        loop.run_until_complete(asyncio.wait(tasks))
-        loop.close()
         return True
 
 
@@ -135,9 +120,11 @@ def _complete_url(baseurl):
 
 
 def func_githack(*args):
+    """GitHack Git源码泄露下载"""
     baseurl = args[0]
     baseurl = _complete_url(baseurl)
     temppath = _repalce_bad_chars(_get_prefix(baseurl))[:-1]
+    os.system("rm -rf ./%s >/dev/null 2>&1" % temppath)
     cprint(CYELLOW, "[!] Base Files Fetching... ")
     for i in BASE_FILES:
         path = "./%s/%s" % (temppath, i)
@@ -154,8 +141,8 @@ def func_githack(*args):
         master = open(_commitFile, "r")
         for line in master:
             _tmp = line.split(" ")
-            pH = tmp[0]
-            nH = tmp[1]
+            pH = _tmp[0]
+            nH = _tmp[1]
             path = "./%s/.git/objects/%s/%s" % (temppath, nH[0:2], nH[2:])
             url = "%sobjects/%s/%s" % (baseurl, nH[0:2], nH[2:])
             try:
@@ -167,12 +154,13 @@ def func_githack(*args):
     _stashFile = "./%s/.git/logs/refs/stash" % temppath
     if not os.path.exists(_stashFile):
         cprint(CRED, "[-] Stash Objects Not Found")
+        _stashFile == False
     else:
         master = open(_stashFile, "r")
         for line in master:
             _tmp = line.split(" ")
-            pH = tmp[0]
-            nH = tmp[1]
+            pH = _tmp[0]
+            nH = _tmp[1]
             path = "./%s/.git/objects/%s/%s" % (temppath, nH[0:2], nH[2:])
             url = "%sobjects/%s/%s" % (baseurl, nH[0:2], nH[2:])
             try:
@@ -185,6 +173,9 @@ def func_githack(*args):
         os.system("cd ./%s && git reset --hard > /dev/null 2>&1" % temppath)
         cprint(CGREEN, "[+] All file downloaded!")
         cprint(CGREEN, "[+] Clone Success. Dist File : ./%s" % temppath)
+        if _stashFile:
+            cprint(CGREEN, "[+] It has Stash [git stash list]")
+
     else:
         cprint(CRED, "[-] Oh My God, .git Not Found")
     return ""
